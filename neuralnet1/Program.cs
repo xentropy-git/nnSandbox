605f882c22d2320cef4fd1a810aa78ccd64c907b
Christopher Laponsie <claponsie@email.davenport.edu>
2021-04-26 12:46:46 -0400

    Implemented activation functions

--- neuralnet1/Program.cs
+++ neuralnet1/Program.cs
@@ -4,36 +4,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * TODO NEXT:
+ *  Implement loss function (categorical cross entropy)
+ *  Implement optimizer
+ */
+
 namespace neuralnet1
 {
+    enum Activation
+    {
+        ReLU,
+        Sigmoid,
+        SoftMax
+    }
+    class ActivationFunction
+    {
+        public double[,] output;
+        public ActivationFunction(int n_neurons)
+        {
+            this.output = new double[1, n_neurons];
+        }
 
-   class DenseLayer
+        public virtual void Forward(double[,] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class ReLU : ActivationFunction
+    {
+        public ReLU(int n_neurons): base (n_neurons)
+        {
+        }
+        public override void Forward (double[,] inputs)
+        {
+            this.output = new double[inputs.GetUpperBound(0)+1, inputs.GetUpperBound(1)+1];
+            for (int i = 0; i < inputs.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < inputs.GetUpperBound(1) + 1; j++)
+                {
+                    this.output[i, j] = Math.Max(0,inputs[i, j]);
+
+                }
+
+            }
+        }
+    }
+
+    class SoftMax : ActivationFunction
+    {
+        public SoftMax(int n_neurons) : base(n_neurons)
+        {
+        }
+        public override void Forward(double[,] inputs)
+        {
+            this.output = new double[inputs.GetUpperBound(0) + 1, inputs.GetUpperBound(1) + 1];
+            for (int i = 0; i < inputs.GetUpperBound(0) + 1; i++)
+            {
+                // calculate the max value for each row
+                double maxVal= 0;
+                for (int j = 0; j < inputs.GetUpperBound(1) + 1; j++)
+                {
+                    if (inputs[i, j] > maxVal) maxVal = inputs[i, j];
+                }
+                
+                // subtract tha max value so the upper limit is 0
+                // output the exponent
+                double rowSum = 0;
+                for (int j = 0; j < inputs.GetUpperBound(1) + 1; j++)
+                {
+                    double newVal = Math.Exp(inputs[i,j]- maxVal);
+                    this.output[i, j] = newVal;
+                    rowSum += newVal;
+                }
+
+                // divide the outputs by the sum to get a percentage
+                // final result is a probability distribution
+                for (int j = 0; j < inputs.GetUpperBound(1) + 1; j++)
+                {
+                    this.output[i, j] = this.output[i, j]/rowSum ;
+                }
+
+            }
+        }
+    }
+    class DenseLayer
     {
         static Random rand = new Random();
-        public float[,] weights;
-        public float[] biases;
-        public float[,] outputs;
-        public DenseLayer (int n_inputs, int n_neurons)
+        public double[,] weights;
+        public double[] biases;
+        public double[,] output;
+        public ActivationFunction activation_function;
+        public DenseLayer (int n_inputs, int n_neurons, Activation at )
         {
             this.weights = random_weights(n_inputs, n_neurons);
-            this.biases = new float[n_neurons];
-            this.outputs = new float[1,n_neurons];
+            this.biases = new double[n_neurons];
+            this.output = new double[1,n_neurons];
+            if (at == Activation.ReLU) this.activation_function = new ReLU(n_neurons);
+            if (at == Activation.SoftMax) this.activation_function = new SoftMax(n_neurons);
+
+
 
 
         }
 
-        public void Forward(float [,] inputs)
+        public void Forward(double [,] inputs)
         {
-            this.outputs = matrix.add(matrix.dot(inputs, this.weights), this.biases);
+            this.output =matrix.add(matrix.dot(inputs, this.weights), this.biases);
+            activation_function.Forward(this.output);
         }
-        static float[,] random_weights (int a, int b)
+        public double [,] GetOutput ()
         {
-            float[,] r = new float[a, b];
+            return activation_function.output;
+        }
+        static double[,] random_weights (int a, int b)
+        {
+            double[,] r = new double[a, b];
             for (int i = 0; i < a; i++)
             {
                 for (int j = 0; j < b; j++)
                 {
-                    r[i, j] = (float)rand.NextDouble()*2-1;
+                    r[i, j] = (double)rand.NextDouble()*2-1;
 
                 }
                 
@@ -72,34 +163,21 @@ namespace neuralnet1
         
         static void Main(string[] args)
         {
+            matrix.unit_tests();
             mnist m = new mnist();
             m.LoadTrainDataset();
 
-            Console.WriteLine("This is C#");
             
-            float[,] inputs = {{ 1.0f, 2.0f, 3.0f, 2.5f },
+            double[,] inputs = {{ 1.0f, 2.0f, 3.0f, 2.5f },
                                {2.0f, 5.0f, -1.0f, 2.0f },
                                {-1.5f, 2.7f, 3.3f, -0.8f } };
-            /*
-            float[,] weights = { { 0.2f, 0.8f, -0.5f, 1.0f },
-                                { 0.5f, -0.91f, 0.26f, -0.5f },
-                                { -0.26f, -0.27f, 0.17f, 0.87f }
-                              };
-            float[] biases = { 2.0f, 3.0f, 0.5f };
-            
-            float[,] outputs = matrix.add(matrix.dot(inputs, matrix.transpose (weights)), biases);
-            
-            Console.WriteLine("{0}, {1}, {2}", outputs[0,0], outputs[0, 1], outputs[0, 2]);
-            Console.WriteLine("{0}, {1}, {2}", outputs[1, 0], outputs[1, 1], outputs[1, 2]);
-            Console.WriteLine("{0}, {1}, {2}", outputs[2, 0], outputs[2, 1], outputs[2, 2]);
-            Console.WriteLine(outputs.Length);
-
-
-            Console.WriteLine("Press any key to exit.");
-            */
-            DenseLayer layer1 = new DenseLayer(3,7);
+   
+            DenseLayer layer1 = new DenseLayer(4, 5, Activation.ReLU);
+            DenseLayer layer2 = new DenseLayer(5, 2, Activation.SoftMax);
             layer1.Forward(inputs);
-            Console.WriteLine(layer1);
+            layer2.Forward(layer1.GetOutput());
+            Console.WriteLine(matrix.MatrixToString(layer2.output));
+            Console.WriteLine(matrix.MatrixToString(layer2.GetOutput()));
 
             Console.ReadKey();
         }
--- neuralnet1/matrix.cs
+++ neuralnet1/matrix.cs
@@ -8,10 +8,10 @@ namespace neuralnet1
 {
     public class matrix
     {
-        public static float dot(float[] vector1, float[] vector2)
+        public static double dot(double[] vector1, double[] vector2)
         {
             // calculates dot product on two vectors of the same shape
-            float sum = 0;
+            double sum = 0;
             for (int i = 0; i < vector1.Length; i++)
             {
                 sum += vector1[i] * vector2[i];
@@ -19,13 +19,13 @@ namespace neuralnet1
             return sum;
         }
 
-        public static float[] dot(float[,] matrix1, float[] vector2)
+        public static double[] dot(double[,] matrix1, double[] vector2)
         {
             // calculates dot product on matrix and vector
-            float[] rv = new float[matrix1.GetUpperBound(0) + 1];
+            double[] rv = new double[matrix1.GetUpperBound(0) + 1];
             for (int i = 0; i < matrix1.GetUpperBound(0) + 1; i++)
             {
-                float sum = 0;
+                double sum = 0;
                 for (int j = 0; j < matrix1.GetUpperBound(1) + 1; j++)
                 {
                     sum += matrix1[i, j] * vector2[j];
@@ -35,12 +35,12 @@ namespace neuralnet1
             }
             return rv;
         }
-        public static float[,] transpose(float[,] m1)
+        public static double[,] transpose(double[,] m1)
         {
             // switches rows and columns
             int rows = m1.GetUpperBound(1) + 1;
             int cols = m1.GetUpperBound(0) + 1;
-            float[,] rv = new float[rows, cols];
+            double[,] rv = new double[rows, cols];
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
@@ -50,20 +50,20 @@ namespace neuralnet1
             }
             return rv;
         }
-        public static float[,] dot(float[,] m1, float[,] m2)
+        public static double[,] dot(double[,] m1, double[,] m2)
         {
             // calculates dot product on two matricies
             // matrix (row, column)
             int rows = m1.GetUpperBound(0) + 1;
             int cols = m2.GetUpperBound(1) + 1;
-            float[,] rv = new float[rows, cols];
+            double[,] rv = new double[rows, cols];
 
             for (int i = 0; i < rows; i++)
             {
 
                 for (int j = 0; j < cols; j++)
                 {
-                    float col_sum = 0f;
+                    double col_sum = 0f;
                     for (int k = 0; k < m2.GetUpperBound(0) + 1; k++)
                     {
                         col_sum += m2[k, j] * m1[i, k];
@@ -75,12 +75,30 @@ namespace neuralnet1
 
             return rv;
         }
+        public static bool compare(double[,] m0, double[,] m1)
+        {
+            // calculates dot product on two matricies
+            // matrix (row, column)
+            int rows = m1.GetUpperBound(0) + 1;
+            int cols = m1.GetUpperBound(1) + 1;
 
-        public static float[,] add(float[,] m1, float[] v2)
+            for (int i = 0; i < rows; i++)
+            {
+
+                for (int j = 0; j < cols; j++)
+                {
+                    if (m0[i, j] != m1[i, j]) return false;
+                }
+
+            }
+
+            return true;
+        }
+        public static double[,] add(double[,] m1, double[] v2)
         {
             int rows = m1.GetUpperBound(0) + 1;
             int cols = m1.GetUpperBound(1) + 1;
-            float[,] rv = new float[rows, cols];
+            double[,] rv = new double[rows, cols];
 
             for (int i = 0; i < rows; i++)
             {
@@ -94,16 +112,96 @@ namespace neuralnet1
             }
             return rv;
         }
+        public static string MatrixToString(double[,] m0)
+        {
+            String r = "";
+            r += "{\n";
+            for (int i = 0; i < m0.GetUpperBound(0) + 1; i++)
+            {
+                r += "  {";
+                for (int j = 0; j < m0.GetUpperBound(1) + 1; j++)
+                {
+                    r += m0[i, j];
+                    if (j == m0.GetUpperBound(1))
+                    {
+                        r += "}\n";
+                    }
+                    else
+                    {
+                        r += ", ";
+                    }
 
-        public static float[] add(float[] matrix1, float[] matrix2)
+                }
+
+            }
+            r += "}\n";
+            return r;
+        }
+        public static double[] add(double[] matrix1, double[] matrix2)
         {
             // adds a vector quantity to a matrix
-            float[] rv = new float[matrix1.GetUpperBound(0) + 1];
+            double[] rv = new double[matrix1.GetUpperBound(0) + 1];
             for (int i = 0; i < matrix1.GetUpperBound(0) + 1; i++)
             {
                 rv[i] = matrix1[i] + matrix2[i];
             }
             return rv;
+        }
+        public static void unit_dot_test_0()
+        {
+            double[,] m0 = {{ 1.0f, 7.0f, 3.0f, -1.0f },
+                           {2.0f, 5.0f, -1.0f, 8.0f },
+                           {0f, 1f, 6f, 3f } };
+            double[,] m1 = {{ 7.0f, 3.0f, 1.0f },
+                           {1.0f, 2.0f, -1.0f },
+                           {2.0f, 5.0f, 3.0f },
+                           {3.0f, 1.0f, 0.0f } };
+            double[,] correct = {{17f, 31f, 3f },
+                                {41f, 19f, -6f },
+                                { 22f, 35f, 17f} };
+            double[,] output = dot(m0, m1);
+
+            if (!compare(output, correct))
+            {
+                Console.WriteLine("Error, unexpected result from matrix.dot()");
+                Console.WriteLine(MatrixToString(output));
+            }
+            else
+            {
+                Console.WriteLine("Unit test passed");
+            }
+        }
+        public static void unit_dot_test_1()
+        {
+
+            double[,] m0 = {{1.0f, 2.0f, 3.0f, 2.5f},
+                        {2.0f, 5.0f, -1.0f, 2.0f},
+                        {-1.5f, 2.7f, 3.3f, -0.8f}};
+
+            double[,] m1 = {{1.0f, -2.0f, 3.0f, 1.5f, 1.3f},
+                        {0.9f, 2.3f, 1.8f, -2.0f, 9.0f},
+                        {9.0f, 1.0f, -2.0f, 7.0f, 1.0f},
+                        {-2.0f, 1.3f, 1.7f, 2.4f, 5.6f}};
+             
+            double[,] correct = {{24.8f,  8.849999f, 4.85f, 24.5f, 36.3f},
+                                {-6.5f,  9.1f,  20.4f, -9.2f, 57.8f},
+                                {32.23f, 11.47f, -7.6f,  13.53f, 21.17f}};
+            double[,] output = dot(m0, m1);
+
+            if (!compare(output, correct))
+            {
+                Console.WriteLine("Error, unexpected result from matrix.dot()");
+                Console.WriteLine(MatrixToString(output));
+            }
+            else
+            {
+                Console.WriteLine("Unit test passed");
+            }
+        }
+        public static void unit_tests ()
+        {
+            unit_dot_test_0();
+            unit_dot_test_1();
         }
     }
 }
